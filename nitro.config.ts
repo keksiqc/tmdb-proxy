bf88fe6cb022ba59ea4e7915f3aee3bcfa05e98e
marlonhotz07 <marlonhotz07@gmail.com>
2025-07-03 08:05:06 +0000

    chore: add compatibility date

--- nitro.config.ts
+++ nitro.config.ts
@@ -2,6 +2,7 @@ import process from 'node:process'
 import { defineNitroConfig } from 'nitropack/config'
 
 export default defineNitroConfig({
+  compatibilityDate: '2025-07-03',
   routeRules: {
     '/**': { cors: true, swr: 3600 },
   },
