d313bcf816f875101e8bc5013eb65134bd9ae322
marlonhotz07 <marlonhotz07@gmail.com>
2025-07-03 08:01:55 +0000

    chore: add nitro config

--- nitro.config.ts
+++ nitro.config.ts
@@ -1,3 +1,13 @@
-//https://nitro.unjs.io/config
+import process from 'node:process'
+import { defineNitroConfig } from 'nitropack/config'
+
 export default defineNitroConfig({
-});
+  routeRules: {
+    '/**': { cors: true, swr: 3600 },
+  },
+  runtimeConfig: {
+    tmdb: {
+      apiKey: process.env.TMDB_API_KEY || '',
+    },
+  },
+})
